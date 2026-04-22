5effb5f53c4fa8a14eeab596590e52352a3326e4
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2026-04-22 20:57:18 +0300

    feat(homepage): add FaqSection with shadcn accordion

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -9,6 +9,7 @@ import { StatsSection } from './_components/StatsSection'
 import { ActionTypesSection } from './_components/ActionTypesSection'
 import { EventsSection } from './_components/EventsSection'
 import { OrganizationsSection } from './_components/OrganizationsSection'
+import { FaqSection } from './_components/FaqSection'
 
 export const metadata: Metadata = {
   title: 'Acasă',
@@ -29,6 +30,7 @@ export default async function HomePage() {
       <ActionTypesSection />
       <EventsSection events={events} />
       <OrganizationsSection orgs={orgs} />
+      <FaqSection />
     </main>
   )
 }
