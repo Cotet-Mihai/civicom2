e9d38875488f021cf226c6c16a363b0f5f55391b
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2026-04-29 09:44:08 +0300

    fix(seo): simplify robots rules, add /auth/callback to disallow

--- app/robots.ts
+++ app/robots.ts
@@ -3,13 +3,11 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: ['/', '/evenimente', '/organizatii'],
-        disallow: ['/panou', '/profil', '/creeaza', '/organizatie', '/admin', '/autentificare', '/inregistrare', '/reseteaza-parola'],
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: ['/', '/evenimente', '/organizatii'],
+      disallow: ['/panou', '/profil', '/creeaza', '/organizatie', '/admin', '/autentificare', '/inregistrare', '/reseteaza-parola', '/auth/callback'],
+    },
     sitemap: 'https://civicom.ro/sitemap.xml',
   }
 }
